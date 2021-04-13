56a43ff7f5ffcaaff7a27710364cd3f78da4f870
Mikhail Silaev <gheljenor@yandex-team.ru>
2021-04-13 18:21:12 +0300

    fix: types declaration

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,14 @@
 declare module 'fast-json-stable-stringify' {
-  function stringify(obj: any): string;
-  export = stringify;
+    type CmpOperand<T, K extends keyof T> = {
+        key: K,
+        value: T[K],
+    }
+
+    type Options<T> = Partial<{
+        cmp: <KA extends keyof T, KB extends keyof T>(a: CmpOperand<T, KA>, b: CmpOperand<T, KB>) => number,
+        cycles: Boolean,
+    }>
+
+    function stringify<T extends object>(obj: T, opts?: Options<T>): string;
+    export = stringify;
 }
